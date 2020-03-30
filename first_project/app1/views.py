d44464c7d2a2470ba665530b3d93a98c1426a808
rianvpn <60006879+rianvpn@users.noreply.github.com>
2020-03-30 13:24:52 +0700

    update template

--- first_project/app1/views.py
+++ first_project/app1/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, HttpResponse
 def hello_world(request):
     return HttpResponse("<h1>Selamat datang</h1>")
 def page1(request):
-    return HttpResponse("<h1>Ini adalah Page1</h1>")
+    return render(request, "page1.html")
 def page2(request):
     return HttpResponse("<h1>Ini adalah Page2</h1>")
